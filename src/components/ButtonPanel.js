a7f5cf46a4bdbc836028adefb18118acf641b345
Rodolfo <brsrkdev@gmail.com>
2020-02-12 15:23:21 -0600

    Fix logic, Add component interaction

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,39 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import './ButtonPanel.css';
 
-const buttonPanel = () => (
-  <div className="buttonPanel">
-    <div className="group-1">
-      <Button name="AC" background="#e0e0e0" />
-      <Button name="+/-" background="#e0e0e0" />
-      <Button name="%" background="#e0e0e0" />
-      <Button name="÷" />
-    </div>
-    <div className="group-2">
-      <Button name="7" background="#e0e0e0" />
-      <Button name="8" background="#e0e0e0" />
-      <Button name="9" background="#e0e0e0" />
-      <Button name="X" />
-    </div>
-    <div className="group-3">
-      <Button name="4" background="#e0e0e0" />
-      <Button name="5" background="#e0e0e0" />
-      <Button name="6" background="#e0e0e0" />
-      <Button name="-" />
-    </div>
-    <div className="group-4">
-      <Button name="1" background="#e0e0e0" />
-      <Button name="2" background="#e0e0e0" />
-      <Button name="3" background="#e0e0e0" />
-      <Button name="+" />
-    </div>
-    <div className="group-5">
-      <Button name="0" background="#e0e0e0" wide />
-      <Button name="." background="#e0e0e0" />
-      <Button name="=" />
-    </div>
-  </div>
-);
+class buttonPanel extends Component {
+  handleClick = btnName => {
+    const { clickHandler } = this.props;
+    clickHandler(btnName);
+  };
+
+  render() {
+    return (
+      <div className="buttonPanel">
+        <div className="group-1">
+          <Button name="AC" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="+/-" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="%" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="÷" clickHandler={this.handleClick} />
+        </div>
+        <div className="group-2">
+          <Button name="7" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="8" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="9" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="X" clickHandler={this.handleClick} />
+        </div>
+        <div className="group-3">
+          <Button name="4" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="5" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="6" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="-" clickHandler={this.handleClick} />
+        </div>
+        <div className="group-4">
+          <Button name="1" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="2" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="3" background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="+" clickHandler={this.handleClick} />
+        </div>
+        <div className="group-5">
+          <Button name="0" background="#e0e0e0" wide clickHandler={this.handleClick} />
+          <Button name="." background="#e0e0e0" clickHandler={this.handleClick} />
+          <Button name="=" clickHandler={this.handleClick} />
+        </div>
+      </div>
+    );
+  }
+}
+
+buttonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default buttonPanel;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,20 +1,20 @@
-import Big from 'big';
+import Big from 'big.js';
 
 const operate = (number1, number2, operation) => {
   const num1 = Big(number1);
   const num2 = Big(number2);
   switch (operation) {
     case '+':
-      return num1.add(num2);
+      return num1.plus(num2).toString();
     case '-':
-      return num1.minus(num2);
+      return num1.minus(num2).toString();
     case 'X':
-      return num1.mul(num2);
+      return num1.mul(num2).toString();
     case '/':
       if (num2 === 0) return "Can't divide by zero";
-      return num1.div(num2);
+      return num1.div(num2).toString();
     case '%':
-      return num1.mod(num2);
+      return num1.mod(num2).toString();
     default:
       return 0;
   }
